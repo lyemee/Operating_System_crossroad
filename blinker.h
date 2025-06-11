a1c6441cae4806a79c76d689d481735c5d43cad6
lyemee <128028246+lyemee@users.noreply.github.com>
2025-06-11 13:37:45 +0900

    Update blinker.h

--- blinker.h
+++ blinker.h
@@ -1,15 +1,30 @@
 #ifndef __PROJECTS_PROJECT2_BLINKER_H__
 #define __PROJECTS_PROJECT2_BLINKER_H__
 
+#include "threads/synch.h"
+#include "projects/crossroads/vehicle.h" // vehicle_info 사용을 위해 필요
+
 /** you can change the number of blinkers */
 #define NUM_BLINKER 4
 
-struct blinker_info {
+enum blinker_dir
+{
+    BLINKER_EW,
+    BLINKER_NS
+};
+
+struct blinker_info
+{
     struct lock **map_locks;
     struct vehicle_info *vehicles;
 };
 
-void init_blinker(struct blinker_info* blinkers, struct lock **map_locks, struct vehicle_info * vehicle_info);
+void init_blinker(struct blinker_info *blinkers, struct lock **map_locks, struct vehicle_info *vehicle_info);
 void start_blinker();
+
+// 외부 참조용 전역 변수 선언
+extern enum blinker_dir current_blinker_state;
+extern struct lock blinker_lock;
+extern struct condition blinker_cond;
 
 #endif /* __PROJECTS_PROJECT2_BLINKER_H__ */
